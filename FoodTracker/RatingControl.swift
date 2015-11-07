7de7b7f4dae8b451b2f832710c3e6f1235904356
Yuki Kobayashi <yuki10.kb@outlook.com>
2015-11-07 22:25:57 +0900

    initial commit

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,18 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by kobayashiyuki on 2015/11/07.
+//  Copyright © 2015年 kobayashiyuki. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIView {
+
+    // MARK: Initialization
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+}
